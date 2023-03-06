cd670e79bbb3bf9b44e3596d28d9da461bbb4a4b
samethub <117895958+samethub@users.noreply.github.com>
2023-03-06 13:00:54 +0300

    Update date_windspeed.py

--- 2th_module/all_graphs/date_windspeed.py
+++ 2th_module/all_graphs/date_windspeed.py
@@ -17,7 +17,7 @@ ax.xaxis.set_major_locator(mdates.DayLocator(interval=3))
 
 
 ax.set_ylabel("Wind Speed (m/s)", fontfamily='serif', fontweight='bold', fontsize=16, color='#051367')
-ax.set_title("Wind Speed in May 2020", fontfamily='serif', fontweight='bold', fontsize=20, color='#051367')
+ax.set_title("Нижний Новгород\nWind Speed in May 2020", fontfamily='serif', fontweight='bold', fontsize=20, color='#051367')
 
 plt.xticks(rotation=-20)
 
